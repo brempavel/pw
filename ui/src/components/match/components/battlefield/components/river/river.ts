71212db79e993ecb8474ca92544b132705bb83fb
george looshch <looshch@loosh.ch>
2024-07-07 13:38:50 +0100

    refactor
    Rename with clearer names, re-organise code.

--- ui/src/components/match/components/battlefield/components/river/river.ts
+++ ui/src/components/match/components/battlefield/components/river/river.ts
@@ -11,10 +11,11 @@ import {
 import { Component } from '@types'
 import { debounce } from '@utils'
 import {
-  WATER_DISPLACEMENT_FILTER_SCALE,
-  WATER_SPRITE_TILE_POSITION_SPEED_DENOMINATOR,
-  WATER_TEXTURE_URL,
-  WATER_WIDTH_IN_PERCENT,
+  RIVER_DISPLACEMENT_URL,
+  RIVER_DISPLACEMENT_FILTER_SCALE,
+  RIVER_SPRITE_TILE_POSITION_SPEED_DENOMINATOR,
+  RIVER_TEXTURE_URL,
+  RIVER_WIDTH_IN_PERCENT,
 } from '@constants'
 
 const DISPLACEMENT_FILTER_COMPENSATION_IN_PERCENT = 30
@@ -23,7 +24,7 @@ export class River extends Component {
   private get width(): number {
     return (
       this.sceneManager.width *
-      ((WATER_WIDTH_IN_PERCENT + DISPLACEMENT_FILTER_COMPENSATION_IN_PERCENT) /
+      ((RIVER_WIDTH_IN_PERCENT + DISPLACEMENT_FILTER_COMPENSATION_IN_PERCENT) /
         100)
     )
   }
@@ -37,18 +38,18 @@ export class River extends Component {
 
   async build(container: Container): Promise<River> {
     const [water] = await Promise.all([
-      await Assets.load<Texture>(WATER_TEXTURE_URL),
-      await Assets.load<Texture>('assets/water-displacement.png'),
+      await Assets.load<Texture>(RIVER_TEXTURE_URL),
+      await Assets.load<Texture>(RIVER_DISPLACEMENT_URL),
     ])
 
     this.sprite = new TilingSprite({ texture: water })
     this.onResize()
 
-    const displacementSprite = Sprite.from('assets/water-displacement.png')
+    const displacementSprite = Sprite.from('assets/river-displacement.png')
     displacementSprite.texture.source.addressMode = 'repeat'
     this.displacementFilter = new DisplacementFilter({
       sprite: displacementSprite,
-      scale: WATER_DISPLACEMENT_FILTER_SCALE,
+      scale: RIVER_DISPLACEMENT_FILTER_SCALE,
     })
 
     this.sprite.filters = [this.displacementFilter]
@@ -58,7 +59,7 @@ export class River extends Component {
     this.onTick = (ticker: Ticker) => {
       if (this.sprite) {
         this.sprite.tilePosition.y +=
-          ticker.deltaMS / WATER_SPRITE_TILE_POSITION_SPEED_DENOMINATOR
+          ticker.deltaMS / RIVER_SPRITE_TILE_POSITION_SPEED_DENOMINATOR
       }
     }
 
--- ui/src/components/match/match.ts
+++ ui/src/components/match/match.ts
@@ -7,10 +7,11 @@ import { Battlefield } from './components'
 export class Match extends Component {
   async build(container: Container): Promise<Battlefield> {
     const match = new Container()
-    container.addChild(match)
 
     const battlefield = await new Battlefield(this.sceneManager).build(match)
     this.onTick = battlefield.onTick
+
+    container.addChild(match)
 
     return this
   }
